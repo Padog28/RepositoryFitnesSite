82762b70a8c18765c4caddc99b59dae08d81a4a8
NickPek <nikitapekisev@gmail.com>
2022-11-13 23:27:06 +0300

    добавил адрес обработчика

--- DataBase/reg.php
+++ DataBase/reg.php
@@ -4,7 +4,9 @@
   </head>
   <body>
     <h2>Регистрация</h2>
-    <form action="">
+    <form action="save_user.php" method="post">
+<!-- save_user.php - это адрес обработчика.  То есть, после нажатия на кнопку "Зарегистрироваться", 
+данные из полей отправятся на страничку save_user.php методом "post" -->
       <p>
         <label>Ваш логин:<br></label>
         <input name="login" type="text" size="15" maxlength="15">
@@ -17,6 +19,7 @@
 <!-- В поле для паролей (name="password" type="password") пользователь вводит свой пароль -->  
       <p>
         <input type="submit" name="submit" value="Зарегистрироваться">
+<!-- Кнопочка (type="submit") отправляет данные на страничку save_user.php -->
       </p>
     </form>
   </body>
