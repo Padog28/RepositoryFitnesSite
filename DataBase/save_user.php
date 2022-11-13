948900b57b34faa09ecf69efdabb67a8773a9ee1
NickPek <nikitapekisev@gmail.com>
2022-11-13 23:22:21 +0300

    обработка логина и пароля

--- DataBase/save_user.php
+++ DataBase/save_user.php
@@ -1,27 +1,38 @@
 <?php
-     //заносим введенный пользователем логин в переменную $login, если он пустой, то уничтожаем переменную
-     if (isset($_POST['login'])) 
-          { 
-               $login = $_POST['login']; 
-               if ($login == '') 
-               { 
-                    unset($login);
-               } 
-          } 
+    //заносим введенный пользователем логин в переменную $login, если он пустой, то уничтожаем переменную
+    if (isset($_POST['login'])) 
+    { 
+        $login = $_POST['login']; 
+        if ($login == '') 
+        { 
+            unset($login);
+        } 
+    } 
 
-     //заносим введенный пользователем пароль в переменную $password, если он пустой, то уничтожаем переменную
-     if (isset($_POST['password'])) 
-          { 
-               $password=$_POST['password']; 
-               if ($password =='') 
-                    {
-                         unset($password);
-                    } 
-          }
-          
-     //если пользователь не ввел логин или пароль, то выдаем ошибку и останавливаем скрипт
-     if (empty($login) or empty($password)) 
-          {
-               exit ("Вы ввели не всю информацию, вернитесь назад и заполните все поля!");
-          } 
+    //заносим введенный пользователем пароль в переменную $password, если он пустой, то уничтожаем переменную
+    if (isset($_POST['password'])) 
+    { 
+        $password=$_POST['password']; 
+        if ($password =='') 
+        {
+            unset($password);
+        } 
+    }
+
+    //если пользователь не ввел логин или пароль, то выдаем ошибку и останавливаем скрипт
+    if (empty($login) or empty($password)) 
+    {
+        exit ("Вы ввели не всю информацию, вернитесь назад и заполните все поля!");
+    } 
+
+    //если логин и пароль введены, то обрабатываем их, чтобы теги и скрипты не работали, мало ли что люди могут ввести
+    $login = stripslashes($login);
+    $login = htmlspecialchars($login);
+
+    $password = stripslashes($password);
+    $password = htmlspecialchars($password);
+
+    //удаляем лишние пробелы
+    $login = trim($login);
+    $password = trim($password);
 ?>
